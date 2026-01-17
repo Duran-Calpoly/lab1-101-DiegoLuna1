63ebaf2c60c90f3957c34091df7468a2103e84c0
DiegoLuna1 <diegoalexluna1@gmail.com>
2026-01-17 01:00:04 +0000

    Lab 1 Done

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,9 @@
-pass
+def calculate_average (num1, num2, num3):
+    totalaverage = (num1 + num2 + num3)/3
+    return totalaverage
+def add_tax (bill_total):
+    cost = bill_total + (bill_total * 0.1)
+    return cost
+def greet_user (name):
+    greeting = "Hello " + name
+    return greeting
--- lab1_5.py
+++ lab1_5.py
@@ -1,1 +1,21 @@
-pass
+def check_multiple (number):
+    if number % 3 == 0 and number % 5 == 0:
+        return True
+    else:
+        return False
+def check_password (input_string):
+    secret_word = "Python123"
+    if secret_word == input_string:
+        return "access granted"
+    else:
+        return "access denied"
+def calculate_federal_tax (salary):
+    if salary <= 11000:
+        return salary * 0.10
+    elif salary <= 44725:
+        return salary * 0.12
+    elif salary <= 95375:
+        return salary * 0.22
+    elif salary > 95375: 
+        return salary * 0.24
+
